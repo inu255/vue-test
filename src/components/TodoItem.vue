5f2e9dc86cbb14f0c8ba32a6ed049c93b465d1fc
inu255 <artmvhdv@mail.ru>
2020-12-24 14:10:32 +0700

    TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="todo-item">
+    <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TodoItem',
+    props: ['todo']
+  }
+</script>
+
+<style lang="css" scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px dotted #ccc;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    cursor: pointer;
+    float: right;
+  }
+</style>
